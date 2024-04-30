b1294f85b900d16304c44d5fc8f8c31633a9de10
kangddong <rkdehddud96@naver.com>
2024-04-30 20:28:20 +0900

    Initial Commit

---
+++ Proteini/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Proteini
+//
+//  Created by 강동영 on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Proteini/ProteiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProteiniApp.swift
+//  Proteini
+//
+//  Created by 강동영 on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ProteiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
